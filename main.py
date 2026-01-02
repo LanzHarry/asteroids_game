f1f181dbcaba4ad225e27490f87cdc9917660285
Harry Lanz <lanzharry97@gmail.com>
2026-01-02 11:53:07 +0000

    add player sprite and draw to screen

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         log_state()
@@ -16,10 +19,10 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = game_clock.tick(60)/1000
-        print(dt)
 
 
 
