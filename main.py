adaf17b427d4a7081202a1dd5b93e25923640684
Harry Lanz <lanzharry97@gmail.com>
2026-01-02 11:33:32 +0000

    add framerate limit

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -15,6 +17,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = game_clock.tick(60)/1000
+        print(dt)
 
 
 
